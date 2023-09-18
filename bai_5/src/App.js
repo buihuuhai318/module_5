9db2b6c51502262e40fb11ea5d26f629b998f2b2
buihuuhai318 <131940749+buihuuhai318@users.noreply.github.com>
2023-09-18 21:14:26 +0700

    oke

---
+++ bai_5/src/App.js
@@ -0,0 +1,41 @@
+import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
+import React, {useState} from 'react';
+
+function App() {
+    const [list, setList] = useState([])
+    const [item, setItem] = useState('')
+
+    const handleChange = (event) => {
+        setItem(event.target.value);
+    }
+
+    const handleAddItem = () => {
+        if (item.trim() !== '') {
+            setList([...list, item]);
+            setItem('');
+        }
+    }
+    return (
+        <div style={{ width: '20%', marginLeft: 'auto', marginRight: 'auto' , marginTop: '5%'}}>
+            <h1>Todo List</h1>
+            <div className="input-group mb-3">
+                <input type="text" className="form-control" placeholder="Enter a task" aria-label="Recipient's username"
+                       aria-describedby="button-addon2" value={item}
+                       onChange={handleChange}/>
+                    <button className="btn btn-outline-secondary" type="button" id="button-addon2"
+                            onClick={handleAddItem}>Add
+                    </button>
+            </div>
+            <ul>
+                {list.map((task, index) => (
+                    <li key={index}>{task}</li>
+                ))}
+            </ul>
+        </div>
+
+
+);
+}
+
+export default App;
