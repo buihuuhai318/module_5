69de05ae12df2e2818c7e36226580b756ad327e1
buihuuhai318 <131940749+buihuuhai318@users.noreply.github.com>
2023-09-20 14:07:56 +0700

    oke

--- case/src/ServiceIndex.js
+++ case/src/ServiceIndex.js
@@ -1,40 +1,82 @@
 import "bootstrap/dist/css/bootstrap.css";
 
-function Header() {
+function ServiceIndex() {
     return (
         <>
-            <nav className="navbar navbar-expand-lg  sticky-top" style={{backgroundColor: 'aquamarine'}}>
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="#">#Thehome</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#navbarNavDropdown"
-                            aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNavDropdown">
-                        <ul className="navbar-nav">
-                            <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="">Home</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="">Customer</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="">Contract</a>
-                            </li>
-                        </ul>
+            <div className="row" style={{margin: '5% 12% 5% 12%'}}>
+                <div style={{width: '4%', position: 'fixed', right: '80px', top: '90vh', zIndex: '9999'}}>
+                    <a href="createService.html" type="button" className="btn btn-outline-success"
+                       style={{borderRadius: '30%'}}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor"
+                             className="bi bi-plus" viewBox="0 0 16 16">
+                            <path
+                                d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+
+            <div className="row" style={{margin: '5% 12% 5% 12%', display: 'flex'}}>
+                <div className="col-xl-4 col-sm-6 col-12" style={{display: 'flex', marginBottom: '3%'}}>
+                    <div className="card" style={{width: '23rem'}}>
+                        <img src="https://i.imgur.com/HuUb5GT.jpg" className="card-img-top" alt="..."/>
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up
+                                the bulk of the
+                                card's content.</p>
+                            <div className="text-end">
+                                <a href="#" className="btn btn-primary">View</a>
+                                <button type="button" className="btn btn-danger" data-bs-toggle="modal"
+                                        data-bs-target="#exampleModal">
+                                    Delete
+                                </button>
+                            </div>
+                            <div className="modal fade" id="exampleModal" tabIndex="-1"
+                                 aria-labelledby="exampleModalLabel"
+                                 aria-hidden="true">
+                                <div className="modal-dialog">
+                                    <div className="modal-content">
+                                        <div className="modal-header">
+                                            <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                                            <button type="button" className="btn-close" data-bs-dismiss="modal"
+                                                    aria-label="Close"></button>
+                                        </div>
+                                        <div className="modal-body">
+                                            ...
+                                        </div>
+                                        <div className="modal-footer">
+                                            <button type="button" className="btn btn-secondary"
+                                                    data-bs-dismiss="modal">Close
+                                            </button>
+                                            <button type="button" className="btn btn-danger">Delete</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </nav>
-            <div className="card text-bg-dark">
-                <img src="https://i.imgur.com/TRKHgp5.jpg" className="card-img" style={{height: '100%'}} alt="..."/>
-                    <div className="card-img-overlay"
-                         style={{ textAlign: 'center', top: '300px', color: 'black', fontSize: '200%' }}>
-                        <h1 className="card-title">#Thehome - Resort</h1>
-                    </div>
+                <nav aria-label="Page navigation example">
+                    <ul className="pagination" style={{marginLeft: '37%'}}>
+                        <li className="page-item">
+                            <a className="page-link" href="#" aria-label="Previous">
+                                <span aria-hidden="true">&laquo;</span>
+                            </a>
+                        </li>
+                        <li className="page-item"><a className="page-link" href="#">1</a></li>
+                        <li className="page-item"><a className="page-link" href="#">2</a></li>
+                        <li className="page-item"><a className="page-link" href="#">3</a></li>
+                        <li className="page-item">
+                            <a className="page-link" href="#" aria-label="Next">
+                                <span aria-hidden="true">&raquo;</span>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
             </div>
         </>
-);
+    );
 }
 
-export default Header;
+export default ServiceIndex;
