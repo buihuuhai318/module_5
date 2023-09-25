ffc72288709e6544e13af901dd2cc25ae09d4fe1
buihuuhai318 <131940749+buihuuhai318@users.noreply.github.com>
2023-09-25 21:02:37 +0700

    ok

---
+++ redux-saga/src/components/User.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+function User() {
+    const users = useSelector(state => state.users);
+    console.log(users);
+    function deleteUser(id) {
+
+    }
+    return (
+        <div>
+            <table>
+                <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Website</th>
+                    <th>Action</th>
+                </tr>
+                </thead>
+                <tbody>
+                {users.map(user => (
+                    <tr key={user.id}>
+                        <td>{user.id}</td>
+                        <td>{user.username}</td>
+                        <td>{user.email}</td>
+                        <td>{user.website}</td>
+                        <td><button type="button" onClick={() => deleteUser(user.id)}>delete</button></td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </div>
+    );
+}
+export default User;
